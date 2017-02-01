315b4feaec6e7e86f915af305e30319be07b1965
Jacob Hedengren <=>
2017-02-01 23:38:06 +0100

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :contact
-  resources :for_employers
-  resources :about
-  resources :employers
-  resources :jobs
+  
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'jobs#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
