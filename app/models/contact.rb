ca8ed37700eb6552e82effd36682c269ecb49c3b
Jacob Hedengren <=>
2017-02-12 23:21:36 +0100

    test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,9 @@ class Contact < MailForm::Base
   attribute :attachment,   :validate => true
   attribute :nickname,  :captcha  => true
 
+  attachment :content_type => "image/jpeg",
+      :body => File.read("an-image.jpg")
+
   def headers
     {
       :subject => "Contact Form",
