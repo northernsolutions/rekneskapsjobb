ca5ac5e7729b78e8b3839a4cbe39c6c84ef55790
Jacob Hedengren <=>
2017-02-12 23:54:05 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :contacts, only: [:new, :create]
-  resources :contact
-  resources :for_employers
-  resources :about
-  resources :employers
-  resources :jobs do
-    collection do
-      get :search
-    end
-  end
 
-  root 'jobs#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
