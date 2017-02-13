178faa2788ad3ad4646eb5eadcd07a286619422e
Jacob Hedengren <=>
2017-02-13 16:41:21 +0100

    test form

---
+++ app/helpers/let_the_job_find_you_helper.rb
@@ -0,0 +1,2 @@
+module LetTheJobFindYouHelper
+end
---
+++ test/controllers/let_the_job_find_you_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LetTheJobFindYouControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
